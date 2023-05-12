d8fbea038e49851e0f46aa684b0db58187a3547a
Geopozitcia <119807408+Geopozitcia@users.noreply.github.com>
2023-05-12 17:22:08 +0700

    First commit

---
+++ Course_paper/Course_paper/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Course_paper
+//
+//  Created by Илья Сухаренко on 12.05.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
