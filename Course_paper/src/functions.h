6df4c3ff2e9c7af41fd967fa69187e1c4cfb4bb3
Geopozitcia <119807408+Geopozitcia@users.noreply.github.com>
2023-05-21 20:55:00 +0700

    Fix maches-function and search

--- Course_paper/src/functions.h
+++ Course_paper/src/functions.h
@@ -5,11 +5,12 @@
 #include <string.h>
 #include <sys/types.h>
 #define CSI "\x1B\x5B"
+#define MAX_ABC 256
+#define ASCII_SIZE 255
 
-int matches(char *sourse, int transition_cells[256][256], const size_t len_sample);
-void recursion_search(char *path, int transition_cells[256][256], size_t len_sample, int *match_counter);
-void no_recursion_search(char *path, int transition_cells[256][256], size_t len_sample, int *match_counter);
-void print_table(int transition_cells[256][256], const size_t len_sample, const char *facecontrol);
-void fill_line(const size_t n);
-void table_logic(const char* sample, int transition_cells[256][256], const char* facecontrol);
-
+int matches(const char *sourse, int transition_cells[MAX_ABC][MAX_ABC], const size_t len_sample);
+void recursion_search(char *path, int transition_cells[MAX_ABC][MAX_ABC],const size_t len_sample, int *match_counter);
+void no_recursion_search(char *path, int transition_cells[MAX_ABC][MAX_ABC], const size_t len_sample, int *match_counter);
+void table_logic(const char *sample, int transition_cells[MAX_ABC][MAX_ABC], const char *facecontrol);
+void fill_line(const int n);
+void print_table(int transition_cells[MAX_ABC][MAX_ABC], const size_t len_sample, const char *facecontrol); 
