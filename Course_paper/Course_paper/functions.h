b46389ca523ed5250846d0e5c7bbd4e10575a7f3
Geopozitcia <119807408+Geopozitcia@users.noreply.github.com>
2023-05-21 18:09:42 +0700

    Create makefile and change header for fsmatcher

--- Course_paper/Course_paper/functions.h
+++ Course_paper/Course_paper/functions.h
@@ -9,5 +9,6 @@
 int matches(char *sourse, int transition_cells[256][256], const size_t len_sample);
 void recursion_search(char *path, int transition_cells[256][256], size_t len_sample, int *match_counter);
 void no_recursion_search(char *path, int transition_cells[256][256], size_t len_sample, int *match_counter);
-
-
+void printTable(int transition_cells[256][256], const size_t len_sample, const char *facecontrol);
+void fill_line(const size_t n);
+void table_logic(const char* sample, int transition_cells[256][256], const char* facecontrol);
